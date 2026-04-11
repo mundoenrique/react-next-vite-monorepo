5e79c0792f099620cef8db36909e64084f23765d
Jesús Enrique Peñaloza Piñero <enrique.penaloza.working@gmail.com>
2026-04-11 12:29:28 -0400

    feat(shadcn-ui): crear paquete base compartido y configuración inicial
    - agregar components.json para configuración de generación
    - inicializar package.json con dependencias, exports y sideEffects
    -agregar configuración de eslint y tsconfig del paquete
    - agregar estilos globales y utilidades base (cn)

---
+++ packages/shadcn-ui/eslint.config.mjs
@@ -0,0 +1,5 @@
+import eslintBase from '@repo/eslint-config/eslint.base';
+import eslintReact from '@repo/eslint-config/eslint.react';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([...eslintBase, ...eslintReact, globalIgnores(['./src'])]);
