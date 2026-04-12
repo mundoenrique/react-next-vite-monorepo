083b644a61059d00deaadbf28b085b8ab941e6f7
Jesús Enrique Peñaloza Piñero <enrique.penaloza.working@gmail.com>
2026-04-11 23:07:50 -0400

    refactor(styles): eliminar archivos css no utilizados y actualizar las importaciones de diseño

--- apps/next-app2/src/app/layout.tsx
+++ apps/next-app2/src/app/layout.tsx
@@ -2,8 +2,6 @@ import '@repo/shadcn-ui/globals.css';
 import { type Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
-import './globals.css';
-
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
